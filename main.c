9cc9bc94dbd3dbe7d5bcd31f07001e70276c75d1
Jade Kim <JadeKim.dev@gmail.com>
2022-05-23 23:22:03 +0900

    Initial Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+    typedef uint8_t BYTE;
+
+    // Clion test commit
+
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// Functions (R, G, B)
