4a760042f84abdb00b417170666016a38c3f431b
Jade Kim <JadeKim.dev@gmail.com>
2022-05-26 15:37:19 +0900

    File check

--- filter.c
+++ filter.c
@@ -13,6 +13,19 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Received file names
+    char *in_name = argv[2];
+    char *out_name = argv[3];
+
+    // Open input image & output file
+    FILE *infile = fopen(in_name, "r");
+    FILE *outfile = fopen(out_name, "w");
+    if (infile == NULL || outfile == NULL)
+    {
+        printf("Could not process.\n");
+        return 2;
+    }
+
     return 0;
 }
 
